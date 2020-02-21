13574b465942d286e3f184cef58931ff6a736438
quangnv <nguyenvanquang96th@gmail.com>
2020-02-21 08:12:12 +0700

    Config navigation

--- app/src/main/java/com/example/mediaawesomeapp/MainActivity.kt
+++ app/src/main/java/com/example/mediaawesomeapp/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.mediaawesomeapp
 
 import android.os.Bundle
-import android.view.View
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.updateLayoutParams
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.card.MaterialCardView
+import com.example.mediaawesomeapp.ext.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,18 +10,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupEdgeToEdge()
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // else, need to wait for onRestoreInstanceState
     }
 
-    private fun setupEdgeToEdge() {
-        container.systemUiVisibility =
-            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-        val searchCardMarginTop = resources.getDimensionPixelSize(R.dimen.dp_4)
-        container.setOnApplyWindowInsetsListener { _, insets ->
-            search_toolbar.updateLayoutParams<AppBarLayout.LayoutParams> {
-                topMargin = insets.systemWindowInsetTop + searchCardMarginTop
-            }
-            insets
-        }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val navGraphIds = listOf(
+            R.navigation.nav_my_music,
+            R.navigation.nav_home,
+            R.navigation.nav_chart,
+            R.navigation.nav_mv,
+            R.navigation.nav_following
+        )
+
+        val controller = bottomNavigationView?.setupWithNavController(
+            navGraphIds = navGraphIds,
+            containerId = R.id.nav_host_container,
+            fragmentManager = supportFragmentManager,
+            intent = intent
+        )
+
+        setDefaultSelectedBottomNavigation()
+    }
+
+    private fun setDefaultSelectedBottomNavigation() {
+        bottomNavigationView?.selectedItemId = R.id.nav_home
     }
 }
